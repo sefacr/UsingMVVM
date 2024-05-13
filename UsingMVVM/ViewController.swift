d68f61280abc6bda26a1255043db917de197f27a
sefacr <aacarsefa@gmail.com>
2024-05-13 18:35:17 +0300

    Initial Commit

---
+++ UsingMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsingMVVM
+//
+//  Created by Sefa Acar on 13.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
